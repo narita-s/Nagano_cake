2921182ea93463c3bdf1bb2a8a73bb115367ec70
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-06-04 09:51:05 +0000

    ジャンル一覧画作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -12,7 +12,15 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
+  
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to '/admin/genres'
+  end
+  
 
   private
 
