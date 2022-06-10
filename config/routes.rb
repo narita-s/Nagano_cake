591c6698f5902dda10e038725815974b4cc6a6bc
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-06-10 13:53:37 +0000

    admin/customers

--- config/routes.rb
+++ config/routes.rb
@@ -10,23 +10,17 @@ Rails.application.routes.draw do
     resource :customers, only:[:show, :edit, :update]
   end
   
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  
-
 
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-    namespace :admin do
+  
+  namespace :admin do
     resources :genres, only: [:index, :edit, :create, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
-    
+    resources :customers, only: [:index, :show, :edit, :update]
   end
 
   # 顧客用
