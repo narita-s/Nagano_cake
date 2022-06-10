e303da6e64fc3cffc0b82f34bbf39cf526f090d9
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-06-10 13:46:23 +0000

    レイアウト修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -16,6 +16,17 @@ class Public::CustomersController < ApplicationController
      render :edit
     end
   end
+  
+  def unsubscribe
+    @customer = current_customer
+  end
+
+  def withdrawal
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
 
   private
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,13 +7,13 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  
+
   def full_name
-   last_name + " " + first_name
+   last_name.to_s + " " + first_name.to_s
   end
 
   def full_name_kana
-    last_name_kana + " " + first_name_kana
+    last_name_kana.to_s + " " + first_name_kana.to_s
   end
 
 end
