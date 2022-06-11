0131e0f1a5da0ebe54be8601ae8c145d0ae69c42
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-06-11 09:42:35 +0000

    レイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,22 @@ Rails.application.routes.draw do
 
   # 会員側のルーティング設定
   root to: "public/homes#top"
-  
+
   scope module: :public do
     get "/about", to: "homes#about"
     get "/customers/unsubscribe", to: "customers#unsubscribe"
     patch "/customers/withdrawal", to: "customers#withdrawal"
     resource :customers, only:[:show, :edit, :update]
+    resource :address, only:[:index, :edit, :create, :update, :destroy]
   end
-  
+
 
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
     resources :genres, only: [:index, :edit, :create, :update]
     resources :items, only: [:index, :new, :show, :edit, :create, :update]
