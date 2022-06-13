0fb489c0346ab39d2fd6f575a1383569c9ef0590
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-06-13 01:51:53 +0000

    カート画面作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,26 +7,25 @@ class Public::CartItemsController < ApplicationController
   end
 
     # カート商品を追加・保存
-    def create
-        @cart_item = current_customer.cart_items.new(cart_item_params)
-        # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
-        if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-            #元々カート内にあるもの「item_id」
-            #今追加したparams[:cart_item][:item_id])
-            cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-            cart_item.amount += params[:cart_item][:amount].to_i
-           #cart_item.amountに今追加したparams[:cart_item][:quantity]を加える
-                                                              #.to_iとして数字として扱う
-            cart_item.save
-            redirect_to cart_items_path
-        # もしカート内に「同じ」商品がない場合は通常の保存処理
-        elsif @cart_item.save
-            　@cart_items = current_customer.cart_items.all
-            　render 'index'
-        else　# 保存できなかった場合
-            render 'index'
-        end
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    @cart_items = current_customer.cart_items.all
+    # もし元々カート内に「同じ商品」がある場合、「数量を追加」更新・保存する
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      #元々カート内にあるもの「item_id」
+      #今追加したparams[:cart_item][:item_id])
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      #cart_item.amountに今追加したparams[:cart_item][:quantity]を加える
+      #.to_iとして数字として扱う
+      cart_item.save
+      redirect_to cart_items_path
+    # もしカート内に「同じ」商品がない場合は通常の保存処理
+    else @cart_item.save
+      render 'index'
     end
+  end
+    
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(amount: params[:cart_item][:amount])
@@ -34,9 +33,16 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    @cart_items = CartItem.all
+    render 'index'
   end
 
-  def destroy_all
+  def destroy_all  #カート内全て削除
+    cart_items = CartItem.all
+    cart_items.destroy_all
+    render 'index'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
     resource :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:index, :show]
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create] do
+      collection do
+        delete "destroy_all"
+      end
+    end
     resources :orders, only: [:new, :confirm, :thanks, :create, :index, :show]
   end
 
