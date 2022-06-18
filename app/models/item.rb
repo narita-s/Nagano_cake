d28b48b3c271e6617f15b47004a2eaa4e0af3612
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-06-18 13:02:39 +0000

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
+
   
   def get_image
     unless image.attached?
